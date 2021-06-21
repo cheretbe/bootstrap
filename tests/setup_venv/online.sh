299a1c60b550bb4c64cb839115f43bad01d52977
cheretbe <a.a.orlov@gmail.com>
2021-06-21 16:44:42 +0200

    setup_venv.py: Add ?flush_cache=True option to test script

--- tests/setup_venv/online.sh
+++ tests/setup_venv/online.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 script_dir="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-curl -s https://raw.githubusercontent.com/cheretbe/bootstrap/develop/setup_venv.py | /usr/bin/python3 - test_env --requirement ${script_dir}/requirements.txt --batch-mode
+curl -s https://raw.githubusercontent.com/cheretbe/bootstrap/develop/setup_venv.py?flush_cache=True | /usr/bin/python3 - test_env --requirement ${script_dir}/requirements.txt --batch-mode
