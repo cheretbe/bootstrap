21066cb3c469d6f2f74e08aadfd2af596503e6a5
cheretbe <a.a.orlov@gmail.com>
2021-06-27 16:37:42 +0200

    setup_venv.py: use parameters in online testing script

--- tests/setup_venv/online.sh
+++ tests/setup_venv/online.sh
@@ -6,5 +6,5 @@ script_dir="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 &&
 
 if [ ! -d ${HOME}/.cache/venv/test_env ]; then
   curl -s https://raw.githubusercontent.com/cheretbe/bootstrap/develop/setup_venv.py?flush_cache=True \
-    | /usr/bin/python3 - test_env --requirement ${script_dir}/requirements.txt #--batch-mode
+    | /usr/bin/python3 - test_env --requirement ${script_dir}/requirements.txt "$@"
 fi
